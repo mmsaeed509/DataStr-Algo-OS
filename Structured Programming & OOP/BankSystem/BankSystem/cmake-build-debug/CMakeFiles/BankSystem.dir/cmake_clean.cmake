21acc375f1a26b0a3e3c97d0d0c81958dbd3e38f
Mahmoud Mohammed <62524855+mmsaeed509@users.noreply.github.com>
2021-11-21 17:22:01 +0200

    Add files via upload

---
+++ Structured Programming & OOP/BankSystem/BankSystem/cmake-build-debug/CMakeFiles/BankSystem.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "BankSystem.exe"
+  "BankSystem.exe.manifest"
+  "BankSystem.pdb"
+  "CMakeFiles/BankSystem.dir/AccountOwner.cpp.obj"
+  "CMakeFiles/BankSystem.dir/BankAccount.cpp.obj"
+  "CMakeFiles/BankSystem.dir/CheckingAccount.cpp.obj"
+  "CMakeFiles/BankSystem.dir/SavingsAccount.cpp.obj"
+  "CMakeFiles/BankSystem.dir/main.cpp.obj"
+  "libBankSystem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BankSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
